7601a3d86ed1476d843abeb16ec11f29383db8aa
John Plummer <john@memphis.co.uk>
2017-03-06 18:59:02 +0200

    Add WDS

--- webpack.config.js
+++ webpack.config.js
@@ -6,25 +6,22 @@ const PATHS = {
   dist: path.join(__dirname, 'dist'),
 };
 
-module.exports = {
-  // Entry accepts a path or an object of entries.
-  // We'll be using the latter form given it's
-  // convenient with more complex configurations.
-  //
-  // Entries have to resolve to files! It relies on Node
-  // convention by default so if a directory contains *index.js*, // it will resolve to that.
+const commonConfig = {
   entry: {
     app: PATHS.app,
   },
-
   output: {
     path: PATHS.dist,
     filename: '[name].js',
   },
-
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Webpack demo',
     }),
   ],
+};
+
+module.exports = function (env) {
+  console.log('env', env);
+  return commonConfig;
 };
