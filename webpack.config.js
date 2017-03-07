edbc8c26ee60b44b5d9c1c8d6b4c5994aeac1f6e
John Plummer <john@memphis.co.uk>
2017-03-07 18:12:57 +0200

    Add HMR

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const PATHS = {
@@ -21,7 +22,54 @@ const commonConfig = {
   ],
 };
 
-module.exports = function (env) {
-  console.log('env', env);
+function productionConfig() {
   return commonConfig;
+}
+
+function developmentConfig() {
+  const config = {
+    devServer: {
+      // Enable history API fallback so HTML5 History API based
+      // routing works. This is a good default that will come
+      // in handy in more complicated setups.
+      historyApiFallback: true,
+      // Don't refresh if hot loading fails. If you want
+      // refresh behavior, set hot: true instead.
+      hotOnly: true,
+      // Display only errors to reduce the amount of output.
+      stats: 'errors-only',
+      // Parse host and port from env to allow customization.
+      //
+      // If you use Docker, Vagrant or Cloud9, set
+      // host: options.host || '0.0.0.0';
+      //
+      // 0.0.0.0 is available to all network devices
+      // unlike default `localhost`.
+      host: process.env.HOST, // Defaults to `localhost`
+      port: process.env.PORT, // Defaults to 8080
+      // Enable error/warning overlay
+      overlay: {
+        errors: true,
+        warnings: true,
+      },
+    }, plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NamedModulesPlugin(),
+    ],
+  };
+  return Object.assign(
+    {},
+    commonConfig,
+    config,
+    {
+      plugins: commonConfig.plugins.concat(config.plugins),
+    }
+  );
+}
+
+module.exports = function (env) {
+  if (env === 'production') {
+    return productionConfig();
+  }
+  return developmentConfig();
 };
