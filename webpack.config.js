a27ebbfbf343880b126d5ff913e6bb78ec0712b1
John Plummer <john@memphis.co.uk>
2017-03-14 18:29:30 +0000

    Compose webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,102 +1,48 @@
 const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); const webpack = require('webpack');
+const merge = require('webpack-merge');
+const parts = require('./webpack.parts');
 
 const PATHS = {
-  app: path.join(__dirname, 'app'),
-  dist: path.join(__dirname, 'dist'),
+  app: path.join(__dirname, 'app'), build: path.join(__dirname, 'build'),
 };
 
-const commonConfig = {
-  entry: {
-    app: PATHS.app,
-  },
-  output: {
-    path: PATHS.dist,
-    filename: '[name].js',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Webpack demo',
-    }),
-  ],
-};
-
-function productionConfig() {
-  return commonConfig;
-}
-
-function developmentConfig() {
-  const config = {
-    devServer: {
-      // Enable history API fallback so HTML5 History API based
-      // routing works. This is a good default that will come
-      // in handy in more complicated setups.
-      historyApiFallback: true,
-      // Don't refresh if hot loading fails. If you want
-      // refresh behavior, set hot: true instead.
-      hotOnly: true,
-      // Display only errors to reduce the amount of output.
-      stats: 'errors-only',
-      // Parse host and port from env to allow customization.
-      //
-      // If you use Docker, Vagrant or Cloud9, set
-      // host: options.host || '0.0.0.0';
-      //
-      // 0.0.0.0 is available to all network devices
-      // unlike default `localhost`.
-      host: process.env.HOST, // Defaults to `localhost`
-      port: process.env.PORT, // Defaults to 8080
-      // Enable error/warning overlay
-      overlay: {
-        errors: true,
-        warnings: true,
-      },
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          enforce: 'pre',
-          loader: 'eslint-loader',
-          options: {
-            emitWarning: true,
-          },
-        },
-      ],
+const commonConfig = merge([
+  {
+    entry: {
+      app: PATHS.app,
+    }, output: {
+      path: PATHS.build,
+      filename: '[name].js',
     },
     plugins: [
-      new webpack.HotModuleReplacementPlugin(),
-      new webpack.NamedModulesPlugin(),
-      new webpack.LoaderOptionsPlugin({
-        options: {
-          eslint: {
-            // Fail only on errors failOnWarning: false, failOnError: true,
-            // Disable/enable autofix
-            fix: false,
-            // Output to Jenkins compatible XML
-            outputReport: {
-              filePath: 'checkstyle.xml',
-              formatter: require('eslint/lib/formatters/checkstyle'),
-            },
-          },
-        },
+      new HtmlWebpackPlugin({
+        title: 'Webpack demo',
       }),
     ],
-  };
-  return Object.assign(
-    {},
-    commonConfig,
-    config,
-    {
-      plugins: commonConfig.plugins.concat(config.plugins),
-    }
-  );
-}
+  },
+  parts.lintJavaScript({ include: PATHS.app }),
+]);
+
+const productionConfig = merge([
+]);
+
+const developmentConfig = merge([
+  {
+    plugins: [
+      new webpack.NamedModulesPlugin(),
+    ],
+  },
+  parts.devServer({
+    // Customize host/port here if needed
+    host: process.env.HOST,
+    port: process.env.PORT,
+  }),
+]);
 
 module.exports = function (env) {
   if (env === 'production') {
-    return productionConfig();
+    return merge(commonConfig, productionConfig);
   }
-  return developmentConfig();
+  return merge(commonConfig, developmentConfig);
 };
